28571295d0a5370d5dbe3720cf6d7a0c1fb0c730
Youssef <youssefelgamel12@gmail.com>
2025-04-30 18:27:32 +0300

    Added the rest of classes

---
+++ BookingSystem.java
@@ -0,0 +1,23 @@
+public class BookingSystem {
+    private User users;
+    private Flight flights;
+    private Booking bookings;
+    private Payment payments;
+
+    public BookingSystem(){}
+
+    public BookingSystem(User users, Flight flights, Booking bookings, Payment payments){
+        this.users = users;
+        this.flights = flights;
+        this.bookings = bookings;
+        this.payments = payments;
+    }
+
+    public void searchFlights(){}
+
+    public void createBooking(){}
+
+    public void processPayment(){}
+
+    public void generateTicket(){}
+}
--- Payment.java
+++ Payment.java
@@ -1,3 +1,25 @@
 public class Payment {
-    
+    private int paymentID;
+    private String bookingReference;
+    private double amount;
+    private String method; // paying method
+    private String status;
+    private String transactionDate;
+
+    public Payment(){}
+
+    public Payment(int paymentID, String bookingReference, double amount, String method, String status, String transactionDate){
+        this.paymentID = paymentID;
+        this.bookingReference = bookingReference;
+        this.amount = amount;
+        this.method = method;
+        this.status = status;
+        this.transactionDate = transactionDate;
+    }
+
+    public void processPayment(){}
+
+    public void validatePaymentDetails(){}
+
+    public void updateStatus(){}
 }
