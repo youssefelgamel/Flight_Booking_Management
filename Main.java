53c422ea158ab928d485fa106fb871e07096d9dd
Youssef <youssefelgamel12@gmail.com>
2025-05-01 18:53:01 +0300

    testing

--- Main.java
+++ Main.java
@@ -1,20 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        Agent a = new Agent(1, "agent007", "TopSecret1", "James Bond",
+        Agent a1 = new Agent(1, "agent007", "TopSecret1", "James Bond",
                         "jbond@mi6.co.uk", "+44...", "Intelligence", 0.15);
 
-        Customer c = new Customer(2, "jane_doe", "Pass1234", "Jane Doe",
+        Customer c1 = new Customer(2, "jane_doe", "Pass1234", "Jane Doe",
                         "jane@ex.com", "+20...", "123 Elm St",
                         "BKG123;BKG456", "window seat");
 
-        Administrator adm = new Administrator(3, "root", "Adm1n123", "Root User",
+        Administrator adm1 = new Administrator(3, "root", "Adm1n123", "Root User",
                         "root@sys.local", "+1...", "superuser");
 
+        Customer c2 = new Customer(2,"Dezllar","0123","Youssef Elgamel","youssefelgamel12@gmail.com","01271446218",
+                    "Alexandria","BKG12","2 seats"); // Error in password
+
         String path = "users.csv";
-        a.saveToCsv(path);
-        c.saveToCsv(path);
-        adm.saveToCsv(path);
+        a1.saveToCsv(path);
+        c1.saveToCsv(path);
+        adm1.saveToCsv(path);
     }
 }
 
